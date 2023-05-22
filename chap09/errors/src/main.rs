9fc7889a9d324b1065e9686776fca0a3a28d4dd6
geeeeorge <yuto.george@i.softbank.jp>
2023-05-23 02:01:32 +0900

    refactor: errors 1~10に含まれない条件をかっこよく

--- chap09/errors/src/main.rs
+++ chap09/errors/src/main.rs
@@ -18,7 +18,7 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: u32) -> Guess {
-        if value < 1 || value > 100 {
+        if !(1..=100).contains(&value) {
             panic!("Guess value must be between 1 and 100, got {}.", value);
         }
 
